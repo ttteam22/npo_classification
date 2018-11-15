8d7c64aa4fb80a0fc0778d8dd885971dcc2adea6
tonko22 <tonko22x@gmail.com>
2018-11-15 23:11:24 +0300

    Minor fix

--- parser.py
+++ parser.py
@@ -10,11 +10,10 @@ def get_links(url):
       links.append(link.get('href'))
   return links
 
-def get_text_list()
+def get_text_list():
   url = "http://oppid.ru/"
   html = urlopen(url).read()
   soup = BeautifulSoup(html)
-  result = soup.prettify()
   data = soup.findAll(text=True)
   data_filtered = [each for each in data if each!="\n" and each!=" "]
   return data_filtered
