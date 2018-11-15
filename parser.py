8d7a16875b68af2a99fd6848a670c103784ef2c8
tonko22 <tonko22x@gmail.com>
2018-11-15 23:13:14 +0300

    Minor fix

--- parser.py
+++ parser.py
@@ -3,17 +3,18 @@ from urllib.request import Request, urlopen
 import re
 
 def get_links(url):
-  html = urlopen(url).read()
-  soup = BeautifulSoup(html)
-  links = []
-  for link in soup.findAll('a'):
-      links.append(link.get('href'))
-  return links
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html)
+    links = []
+    for link in soup.findAll('a'):
+        if link!="/":
+            links.append(link.get('href'))
+    return links
 
 def get_text_list():
-  url = "http://oppid.ru/"
-  html = urlopen(url).read()
-  soup = BeautifulSoup(html)
-  data = soup.findAll(text=True)
-  data_filtered = [each for each in data if each!="\n" and each!=" "]
-  return data_filtered
+    url = "http://oppid.ru/"
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html)
+    data = soup.findAll(text=True)
+    data_filtered = [each for each in data if each!="\n" and each!=" "]
+    return data_filtered
