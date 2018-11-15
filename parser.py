05f23c252839dd0a421464e5db7d3a61e6980424
tonko22 <tonko22x@gmail.com>
2018-11-15 23:11:07 +0300

    Added parsing funcs

---
+++ parser.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import re
+
+def get_links(url):
+  html = urlopen(url).read()
+  soup = BeautifulSoup(html)
+  links = []
+  for link in soup.findAll('a'):
+      links.append(link.get('href'))
+  return links
+
+def get_text_list()
+  url = "http://oppid.ru/"
+  html = urlopen(url).read()
+  soup = BeautifulSoup(html)
+  result = soup.prettify()
+  data = soup.findAll(text=True)
+  data_filtered = [each for each in data if each!="\n" and each!=" "]
+  return data_filtered
