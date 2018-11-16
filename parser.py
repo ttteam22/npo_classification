d624332247e37f1ae40d3d53dc6a285adf03467a
tonko22 <tonko22x@gmail.com>
2018-11-16 19:18:52 +0300

    data cleaning updates

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import cyrtranslit
 import re
+from langdetect import detect
 import requests
 
 url = "http://www.rbfondveteranov.ru/"
@@ -32,9 +33,23 @@ def get_text_contents(url) -> list:
     html = r.text
     soup = BeautifulSoup(html)
     data = soup.findAll(text=True)
+    if isinstance(data, str):
+        return [data]
     data_filtered = [each.strip() for each in data if each!="\n" and each!=" "]
     data_filtered = [each for each in data_filtered if each!=""]
     return data_filtered
+
+def get_filtered_text(url):
+    result = []
+    data_filtered = get_text_contents(url)
+    for each in data_filtered:
+        try:
+            if detect(each)=="ru":
+                result.append(each)
+        except:
+            pass
+    return " ".join(result)
+
 
 def get_title(url) -> str:
     return data_filtered
