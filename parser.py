77ba1b29b6817299118d43c56feee5574cf03ea6
tonko22 <tonko22x@gmail.com>
2018-11-16 20:43:10 +0300

    created clean dataset

--- parser.py
+++ parser.py
@@ -29,26 +29,35 @@ def get_links(url):
 
 def get_text_contents(url) -> list:
     # html = urlopen(url).read()
-    r = requests.get(url)
-    html = r.text
-    soup = BeautifulSoup(html)
-    data = soup.findAll(text=True)
-    if isinstance(data, str):
-        return [data]
-    data_filtered = [each.strip() for each in data if each!="\n" and each!=" "]
-    data_filtered = [each for each in data_filtered if each!=""]
-    return data_filtered
+    try:
+        r = requests.get(url, timeout=(5.05, 27))
+        html = r.text
+        soup = BeautifulSoup(html)
+        data = soup.findAll(text=True)
+        if isinstance(data, str):
+            return [data]
+        data_filtered = [each.strip() for each in data if each!="\n" and each!=" "]
+        data_filtered = [each for each in data_filtered if each!=""]
+        return data_filtered
+    except BaseException as e:
+        print("Error: {}, url: {}".format(e, url))
+        return None
 
 def get_filtered_text(url):
     result = []
-    data_filtered = get_text_contents(url)
-    for each in data_filtered:
-        try:
-            if detect(each)=="ru":
-                result.append(each)
-        except:
-            pass
-    return " ".join(result)
+    print(url)
+    try:
+        data_filtered = get_text_contents(url)
+        for each in data_filtered:
+            try:
+                if detect(each)=="ru":
+                    result.append(each)
+            except:
+                pass
+        return " ".join(result)
+    except:
+        print("Error: ", url)
+        return None
 
 
 def get_title(url) -> str:
